ad58bfc9c07d89d41f6fcaa2594612757df21016
Tanner Brown <browntan2003@gmail.com>
2025-07-21 11:06:38 -0500

    First commit. Started this a little while ago but never bothered to add it to git.

---
+++ src/main/java/com/mycompany/calculatorapp/CalculatorApp.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculatorapp;
+
+/**
+ *
+ * @author Scoober
+ */
+public class CalculatorApp {
+
+    public static void main(String[] args) {
+        // this .java file does nothing
+        System.out.println("Hello World!");
+    }
+}
